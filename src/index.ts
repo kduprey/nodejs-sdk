2f5edde944169fab24db0035b88c5b3e3e1eaf13
maxall41 <maxall4@protonmail.com>
2023-07-23 19:37:01 -0700

    Throttle

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+// tslint:disable-next-line:no-var-requires
 const { ofetch } = require('ofetch')
+// tslint:disable-next-line:no-var-requires
+const throttledQueue = require('throttled-queue');
+
+const throttle = throttledQueue(1, 1000); // at most 1 request per second.
 
 export class ByteFlow {
   #API_KEY: string | undefined = undefined;
@@ -7,15 +12,18 @@ export class ByteFlow {
   }
   async sendMessage({ message_content, destination_number }: { message_content: string; destination_number: string }) {
     if (this.#API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
-    return await ofetch('https://api.byteflow.app/sendMessage', {
-      method: 'POST',
-      headers: {
-        api_key: this.#API_KEY,
-      },
-      body: {
-        destination_number: destination_number,
-        message_content: message_content,
-      },
+    throttle(async () => {
+      if (this.#API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
+      await ofetch('https://api.byteflow.app/sendMessage', {
+        method: 'POST',
+        headers: {
+          api_key: this.#API_KEY,
+        },
+        body: {
+          destination_number,
+          message_content,
+        },
+      });
     });
   }
 }
