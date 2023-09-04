9c3979d34587db900b7ffa3b745fbab6b714fc21
Max Campbell <41460735+maxall41@users.noreply.github.com>
2023-09-03 22:33:13 -0700

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,56 @@
-// tslint:disable-next-line:no-var-requires
-const axios = require("axios")
-// tslint:disable-next-line:no-var-requires
-const throttledQueue = require('throttled-queue');
-// tslint:disable-next-line:no-var-requires
-const fs = require("fs")
+import axios from "axios"
+import { readFileSync } from "fs";
+function wait (milliseconds : number) {
+  return new Promise(
+      resolve => setTimeout(resolve, milliseconds)
+  )
+}
+const retry = (axios : any, options = {}) => {
+  return async function (error : any) {
+    if (error.response.status == 420) {
+      await wait(error.response.headers['Retry-After'] * 1000) // Convert seconds to milliseconds
+      return axios(error.config)
+    } else {
+      throw error
+    }
+  }
+}
 
-const throttle = throttledQueue(1, 1000); // at most 1 request per second.
 
 export class ByteFlow {
-  #API_KEY: string | undefined = undefined;
+  private readonly API_KEY: string | undefined = undefined;
+  private readonly client : any | undefined = undefined;
   constructor(API_KEY: string) {
-    this.#API_KEY = API_KEY;
+    this.API_KEY = API_KEY;
+    this.client = axios.create()
+    this.client.interceptors.response.use(null, retry(this.client))
   }
   async sendMessage({ message_content, destination_number }: { message_content: string; destination_number: string }) {
-    if (this.#API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
-    throttle(async () => {
-      if (this.#API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
-      await axios.post('https://api.byteflow.app/sendMessage', {
-        destination_number,
-        message_content,
-      }, {
-        headers: {
-          api_key: this.#API_KEY,
-        },
-      });
+    if (this.API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
+    await this.client.post('https://api.byteflow.app/sendMessage', {
+      destination_number,
+      message_content,
+    }, {
+      headers: {
+        api_key: this.API_KEY,
+      },
     });
   }
   async registerNumber({ phone_number }: { phone_number: string; }) {
-    if (this.#API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
-    throttle(async () => {
-      if (this.#API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
-      await axios.post('https://api.byteflow.app/registerNumber', {
-        phone_number
-      }, {
-        headers: {
-          api_key: this.#API_KEY,
-        },
-      });
+    if (this.API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
+    await this.client.post('https://api.byteflow.app/registerNumber', {
+      phone_number
+    }, {
+      headers: {
+        api_key: this.API_KEY,
+      },
     });
   }
   async lookupPhoneNumber({ phone_number, advanced_mode }: { phone_number: string; advanced_mode: boolean | undefined; }) {
-    if (this.#API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
-    const res = await axios.get(`https://api.byteflow.app/lookupNumber?phone_number=${phone_number}${advanced_mode === true ? "&advanced_mode=true" : ""}`, {
+    if (this.API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
+    const res = await this.client.get(`https://api.byteflow.app/lookupNumber?phone_number=${phone_number}${advanced_mode === true ? "&advanced_mode=true" : ""}`, {
       headers: {
-        api_key: this.#API_KEY,
+        api_key: this.API_KEY,
       },
     });
     return res.data
@@ -53,17 +60,17 @@ export class ByteFlow {
     destination_number: string,
     mediaPath: string
   }){
-    const preSignedURLRequest = await axios.post('https://api.byteflow.app/uploadMedia', {
+    const preSignedURLRequest = await this.client.post('https://api.byteflow.app/uploadMedia', {
       filename: mediaPath.substring(mediaPath.lastIndexOf('/')+1),
     }, {
       headers: {
-        api_key: this.#API_KEY,
+        api_key: this.API_KEY,
       }
     });
     const uploadURL : string = preSignedURLRequest.data.uploadURL;
     const getURL : string = preSignedURLRequest.data.getURL;
-    const file = fs.readFileSync(mediaPath);
-    await axios.put(uploadURL, file);
+    const file = readFileSync(mediaPath);
+    await this.client.put(uploadURL, file);
     await this.sendMessage({
       message_content: message_content + " " + getURL,
       destination_number
