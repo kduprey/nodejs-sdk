797c84d4c622e3b67876d9137ca5485e1c44c86a
Kenton Duprey <kentonduprey@icloud.com>
2023-11-18 18:42:14 -0500

    Adjusting types to follow standard type definition practices, removed 'any' definitions to reduce errors

--- src/types.ts
+++ src/types.ts
@@ -1,31 +1,32 @@
-export interface ISendMessageResponse {
-  sentWithRoute: string | undefined,
-  error: string | any[] | undefined
+export interface SendMessageResponse {
+	sentWithRoute?: string;
+	error?: string | Record<string, unknown>[];
 }
 
-export interface IRegisterNumberResponse {
-  data: string | undefined,
-  error: string | any[] | undefined
+export interface RegisterNumberResponse {
+	data?: string;
+	error?: string | Record<string, unknown>[];
 }
 
-export interface ILookupPhoneNumber {
+export interface LookupPhoneNumber {
   // Basic mode
-  is_voip: boolean,
-  current_carrier_name: string,
-  current_carrier_ocn: string,
+	is_voip: boolean;
+	current_carrier_name: string;
+	current_carrier_ocn: string;
   country: {
-    name: string,
-    dial_code: string,
-    emoji: string,
-    code: string
-  },
+		name: string;
+		dial_code: string;
+		emoji: string;
+		code: string;
+	};
   // Advanced Mode
-  original_ocn: string | undefined,
-  original_carrier_name: string | undefined,
-  ported: boolean | undefined,
-  is_landline: boolean | undefined,
-  is_mobile: boolean | undefined,
-  current_imsi: string | undefined,
-  current_mcc: string | undefined,
-  current_mnc: string | undefined,
+	original_ocn?: string;
+	original_carrier_name?: string;
+	ported?: boolean;
+	is_landline?: boolean;
+	is_mobile?: boolean;
+	current_imsi?: string;
+	current_mcc?: string;
+	current_mnc?: string;
+}
 }
