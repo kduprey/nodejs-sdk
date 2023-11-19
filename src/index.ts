7703221e83f09c90a1b1df4a045ce1b6fa0a0ad6
Kenton Duprey <kentonduprey@icloud.com>
2023-11-18 19:06:34 -0500

    Adding comments to document all classes, methods, and parameters to users of the client

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,34 @@ import type {
 } from './types';
 
 export * from './types';
+
+/**
+ * ByteFlow API Wrapper
+ *
+ * @see {@link https://docs.byteflow.app/} for more information
+ *
+ * @example With JavaScript / CommonJS
+ * ```js
+ * const ByteFlow = require('byteflow');
+ * const byteflow = new ByteFlow('API_KEY');
+ * ```
+ *
+ * @example With TypeScript / ES6
+ * ```ts
+ * import ByteFlow from 'byteflow';
+ * const byteflow = new ByteFlow('API_KEY');
+ * ```
+ *
+ * @param API_KEY - Your ByteFlow API Key
+ * @returns ByteFlow Instance
+ *
+ */
 export class ByteFlow {
 	private readonly BASE_URL = 'https://api.byteflow.app';
-  private readonly API_KEY: string | undefined = undefined;
+	private readonly API_KEY: string | undefined = undefined;
 	private readonly client: AxiosInstance;
-  constructor(API_KEY: string) {
-    this.API_KEY = API_KEY;
+	constructor(API_KEY: string) {
+		this.API_KEY = API_KEY;
 		this.client = axios.create();
 
 		axiosRetry(this.client, {
@@ -36,8 +58,16 @@ export class ByteFlow {
 			},
 		});
 	}
+
+	/**
+	 * Send a message to a number
+	 * @param messageContent - The content of the message you want to send
+	 * @param destinationNumber - The number you want to send the message to
+	 * @returns A promise containing the response from the API - see {@link SendMessageResponse}
+	 */
+
 	public async sendMessage({ messageContent, destinationNumber }: SendMessageParams): Promise<SendMessageResponse> {
-    if (this.API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
+		if (this.API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
 		try {
 			const res = await this.client.post<SendMessageResponse>(
 				`${this.BASE_URL}/sendMessage`,
@@ -46,8 +76,8 @@ export class ByteFlow {
 					message_content: messageContent,
 				},
 				{
-      headers: {
-        api_key: this.API_KEY,
+					headers: {
+						api_key: this.API_KEY,
 						'Retry-Id': nanoid(),
 					},
 					validateStatus: (status) => status < 500,
@@ -59,8 +89,16 @@ export class ByteFlow {
 			console.error(error);
 			throw new Error("Couldn't send message - see logs");
 		}
+	}
+
+	/**
+	 * Register a number
+	 * @param phoneNumber - The number you want to register
+	 * @returns A promise containing the response from the API - see {@link RegisterNumberResponse}
+	 */
+
 	public async registerNumber({ phoneNumber }: RegisterNumberParams): Promise<RegisterNumberResponse> {
-    if (this.API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
+		if (this.API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
 		try {
 			const res = await this.client.post<RegisterNumberResponse>(
 				`${this.BASE_URL}/registerNumber`,
@@ -68,9 +106,9 @@ export class ByteFlow {
 					phone_number: phoneNumber,
 				},
 				{
-      headers: {
-        api_key: this.API_KEY,
-      },
+					headers: {
+						api_key: this.API_KEY,
+					},
 					validateStatus: (status) => status < 500,
 				}
 			);
@@ -80,17 +118,25 @@ export class ByteFlow {
 			throw new Error("Couldn't register number - see logs");
 		}
 	}
+
+	/**
+	 * Lookup a number
+	 * @param phoneNumber - The number you want to lookup
+	 * @param advancedMode - Whether you want to use advanced mode or not
+	 * @returns A promise containing the response from the API - see {@link LookupPhoneNumber}
+	 */
+
 	public async lookupPhoneNumber({ phoneNumber, advancedMode }: LookupPhoneNumberParams): Promise<LookupPhoneNumber> {
-    if (this.API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
+		if (this.API_KEY === undefined) throw new Error('API KEY IS NOT DEFINED');
 		try {
 			const res = await this.client.get<LookupPhoneNumber>(
 				`${this.BASE_URL}/lookupNumber?phone_number=${phoneNumber}${
 					advancedMode === true ? '&advanced_mode=true' : ''
 				}`,
 				{
-      headers: {
-        api_key: this.API_KEY,
-      },
+					headers: {
+						api_key: this.API_KEY,
+					},
 					validateStatus: (status) => status < 500,
 				}
 			);
@@ -99,6 +145,16 @@ export class ByteFlow {
 			console.error(error);
 			throw new Error("Couldn't lookup number - see logs");
 		}
+	}
+
+	/**
+	 * Send a message with media
+	 * @param messageContent - The content of the message you want to send
+	 * @param destinationNumber - The number you want to send the message to
+	 * @param mediaPath - The path to the media you want to send
+	 * @returns A promise containing the response from the API - see {@link SendMessageResponse}
+	 */
+
 	public async sendMessageWithMedia({
 		messageContent,
 		destinationNumber,
@@ -108,19 +164,19 @@ export class ByteFlow {
 			const preSignedURLRequest = await this.client.post<{ uploadURL: string; getURL: string }>(
 				`${this.BASE_URL}/uploadMedia`,
 				{
-      filename: mediaPath.substring(mediaPath.lastIndexOf('/') + 1),
+					filename: mediaPath.substring(mediaPath.lastIndexOf('/') + 1),
 				},
 				{
-      headers: {
-        api_key: this.API_KEY,
+					headers: {
+						api_key: this.API_KEY,
 					},
-      }
+				}
 			);
-    const uploadURL: string = preSignedURLRequest.data.uploadURL;
-    const getURL: string = preSignedURLRequest.data.getURL;
-    const file = readFileSync(mediaPath);
-    await this.client.put(uploadURL, file);
-    const res = await this.sendMessage({
+			const uploadURL: string = preSignedURLRequest.data.uploadURL;
+			const getURL: string = preSignedURLRequest.data.getURL;
+			const file = readFileSync(mediaPath);
+			await this.client.put(uploadURL, file);
+			const res = await this.sendMessage({
 				messageContent: `${messageContent} ${getURL}`,
 				destinationNumber,
 			});
@@ -129,5 +185,5 @@ export class ByteFlow {
 			console.error(error);
 			throw new Error("Couldn't send message with media - see logs");
 		}
-  }
+	}
 }
